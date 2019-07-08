b028ad6a4b637c25cf27f2d910d4c49ba1b2ab2c
Jimmy van Delft <47253443+JimmyvanDelft@users.noreply.github.com>
2019-07-08 13:21:00 +0200

    Add files via upload

---
+++ Calculator/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Dim Number1 As Integer
+    Dim Number2 As Integer
+    Dim Answer As Integer
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Number1 = Num1.Text
+        Number2 = Num2.Text
+        Answer = Number1 + Number2
+        MessageBox.Show(Answer)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Number1 = Num1.Text
+        Number2 = Num2.Text
+        Answer = Number1 - Number2
+        MessageBox.Show(Answer)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Number1 = Num1.Text
+        Number2 = Num2.Text
+        Answer = Number1 * Number2
+        MessageBox.Show(Answer)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Number1 = Num1.Text
+        Number2 = Num2.Text
+        Answer = Number1 / Number2
+        MessageBox.Show(Answer)
+    End Sub
+End Class
